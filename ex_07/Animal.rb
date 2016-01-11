3e6dbaf473763689990812b16d45bca81bd4f9ec
Sambo <losangeles9@gmail.com>
2016-01-11 15:11:41 +0100

    ex_07

---
+++ ex_07/Animal.rb
@@ -0,0 +1,18 @@
+class Animal
+  attr_reader :legs, :sound
+  attr_writer :type
+  attr_accessor :name
+  # Constructor
+  def initialize(legs = 4, sound = "MmmmKROOOOOOOO", type = "Platypus", name = "Martin")
+    @legs = legs
+    @sound = sound
+    @type = type
+    @name = name
+  end
+end
+
+# animal = Animal.new(2, 'Youhou', 'monkey', 'Sambo')
+# puts animal.name
+# puts animal.legs
+# animal.type = "afeuabv"
+# puts animal.sound
